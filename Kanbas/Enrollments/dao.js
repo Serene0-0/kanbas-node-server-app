d9762e39264fdd87578834033639797cf2d3af83
Serene <chenshiyu@CCs-MacBook-Pro.local>
2024-12-08 13:43:25 -0500

    a6

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,20 +1,19 @@
 import Database from "../Database/index.js";
+import model from "./model.js";
 
-export function findAllEnrollments() {
-  return Database.enrollments;
-}
-
-export function findEnrollmentsForUser(userId) {
-  return Database.enrollments.filter((enrollment) => enrollment.user === userId);
-}
+export async function findCoursesForUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
+ }
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
+} 
 
 export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+  return model.create({ userId, courseId });
 }
 
-export function deleteEnrollment(userId, courseId) {
-  Database.enrollments = Database.enrollments.filter(
-    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-  );
-}
+export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+ }
